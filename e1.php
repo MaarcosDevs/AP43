74467744b76ef44d2be0252559f09ed59fade93f
MaarcosDevs <maheru@alumnatflorida.es>
2025-10-27 09:07:40 +0100

    e1 Realizado - mostrar array con vardump

---
+++ e1.php
@@ -0,0 +1,18 @@
+<?php 
+
+$capitals = [
+    ['city' =>"Tokyo"],
+    ['city' =>"Mexico City"],
+    ['city' =>"New York City"],
+    ['city' =>"Mumbai"],
+    ['city' =>"Seoul"],
+    ['city' =>"Shanghai"],
+    ['city' =>"Lagos"],
+    ['city' =>"Buenos Aires"], 
+    ['city' =>"Cairo"],
+    ['city' =>"London"]
+];
+
+var_dump($capitals);
+?>
+
