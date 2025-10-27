e06909c36d979b3295ee260f6ad7cbb89922dc30
MaarcosDevs <maheru@alumnatflorida.es>
2025-10-27 09:13:41 +0100

    Ejercicio 3 Realizado - Utilizar Var_Dump para mostrar

---
+++ e3.php
@@ -0,0 +1,46 @@
+<?php
+$capitals = [
+    [
+        'country' =>"Japan",
+        'city' =>"Tokyo"
+    ],
+    [
+        'country' =>"Mexico",
+        'city' =>"Mexico City"
+    ],
+    [
+        'country' =>"USA",
+        'city' =>"New York City"
+    ],
+    [
+        'country' =>"India",
+        'city' =>"Mumbai"
+    ],
+    [
+        'country' =>"Korea",
+        'city' =>"Seoul"
+    ],
+    [
+        'country' =>"China",
+        'city' =>"Shanghai"
+    ],
+    [
+        'country' =>"Nigeria",
+        'city' =>"Lagos"
+    ],
+    [
+        'country' =>"Argentina", 
+        'city' =>"Buenos Aires" 
+    ],
+    [
+        'country' =>"Egypt",
+        'city' =>"Cairo"
+    ],
+    [
+        'country' =>"England",
+        'city' =>"London"
+    ]
+];
+
+var_dump($capitals);
+?>
