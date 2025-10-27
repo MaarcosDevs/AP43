679f910e087e86d2b49d58b671ecf2298d878840
MaarcosDevs <maheru@alumnatflorida.es>
2025-10-27 09:10:33 +0100

    e2 Realizado - Mostrar con vardump y corregir 1 error

--- e2.php
+++ e2.php
@@ -1,6 +1,17 @@
 <?php 
 
-
-
+$capitals = [
+    ['country' =>"Japan"],
+    ['country' =>"Mexico"],
+    ['country' =>"USA"],
+    ['country' =>"India"],
+    ['country' =>"Korea"],
+    ['country' =>"China"],
+    ['country' =>"Nigeria"],
+    ['country' =>"Argentina"], 
+    ['country' =>"Egypt"],
+    ['country' =>"England"]
+];
+var_dump($capitals);
 ?>
 
